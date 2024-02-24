b784bada5585f610f42e9feaf9b8aa561a0a21c7
David Coats <david@getsafetycone.com>
2024-02-24 00:56:40 -0800

    Update method name.

--- source/R5T.Z0019/Code/Values/IProjectFilePaths.cs
+++ source/R5T.Z0019/Code/Values/IProjectFilePaths.cs
@@ -8,7 +8,7 @@ namespace R5T.Z0019
 	[ValuesMarker]
 	public partial interface IProjectFilePaths : IValuesMarker
 	{
-		public string Example => Instances.ProjectPathsOperator.GetProjectFilePath(
+		public string Example => Instances.ProjectPathsOperator.Get_ProjectFilePath(
 			Instances.DirectoryPaths.Temp,
 			Instances.ProjectNames.Example);
 	}
